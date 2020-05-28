119b70ed89c59a169ce664a193ae436c7815ea3c
Sabina <sabina.abbasova2909@gmail.com>
2020-05-28 19:28:16 +0400

    finished project details

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,22 +1,53 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {firestoreConnect} from 'react-redux-firebase'
+import {compose} from 'redux'
 
 const ProjectDetails = (props) => {
     // console.log(props)
-    const id = props.match.params.id
-    return (
+    // const id = props.match.params.id
+
+    const {project} = props
+
+    if (project) {
+        return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title - {id}</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+                    <span className="card-title">{project.title}</span>
+                    <p>{project.content}</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Sabina Abbasova</div>
+                    <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
                     <div>23rd May, 9PM</div>
                 </div>
             </div>
         </div>
-    )
+        )
+    }
+    
+    else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        )
+    }
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    // console.log(state)
+    const id = ownProps.match.params.id
+    const projects = state.firestore.data.projects
+    const project = projects ? projects[id] : null
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'projects'}
+    ])
+)(ProjectDetails)
