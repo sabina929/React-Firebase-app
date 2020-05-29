a4c4a666d2b01a9f2042b2958e09f692a67cd761
Sabina <sabina.abbasova2909@gmail.com>
2020-05-30 01:39:18 +0400

    finished app

---
+++ dist/precache-manifest.aecf89863b7fac0b2df1c759fa8f931d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5004a675098f83b1857d096d5a6116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffdf4f4e5c683e19f4f3",
+    "url": "/static/css/main.6422b3e8.chunk.css"
+  },
+  {
+    "revision": "cfdac8c46db52adda61f",
+    "url": "/static/js/2.3d2dc680.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.3d2dc680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdf4f4e5c683e19f4f3",
+    "url": "/static/js/main.798df570.chunk.js"
+  },
+  {
+    "revision": "dc468319225a0c657999",
+    "url": "/static/js/runtime-main.e7b89e5a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
@@ -11,7 +11,8 @@ import {Provider, useSelector  } from 'react-redux'
 import thunk from 'redux-thunk'
 
 import { reduxFirestore, getFirestore, createFirestoreInstance} from 'redux-firestore'
-import { reactReduxFirebase, getFirebase, ReactReduxFirebaseProvider, isLoaded  } from 'react-redux-firebase'
+import { getFirebase, ReactReduxFirebaseProvider, isLoaded  } from 'react-redux-firebase'
+// import { reactReduxFirebase, getFirebase, ReactReduxFirebaseProvider, isLoaded  } from 'react-redux-firebase'
 
 import fbConfig from './config/fbConfig'
 import firebase from 'firebase/app'
@@ -103,4 +104,4 @@ function AuthIsLoaded({ children }) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
